d4bb50dac8433cc3c05f8d8138181f450a80ff82
shubhamgupta.mk@gmail.com <shubhamgupta.mk@gmail.com>
2019-03-25 14:44:38 +0000

    new

--- update_academic.sh
+++ update_academic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Display available updates to Academic.
-cd themes/academic
+cd themes/academic @ 5115b1f
 git fetch
 git log --pretty=oneline --abbrev-commit --decorate HEAD..origin/master
 cd ../../
